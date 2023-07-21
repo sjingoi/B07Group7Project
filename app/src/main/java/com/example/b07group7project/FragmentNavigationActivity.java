0006bd9fdc5222fcf85f51df4f07cc6246dfb79d
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-07-20 23:57:01 -0400

    Added Navigation Activity

---
+++ app/src/main/java/com/example/b07group7project/FragmentNavigationActivity.java
@@ -0,0 +1,14 @@
+package com.example.b07group7project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentNavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_navigation);
+    }
+}
