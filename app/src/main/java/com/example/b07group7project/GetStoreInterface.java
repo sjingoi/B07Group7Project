8d8c679e7e76d8ca5ae5efa2930f20f32c78c25d
Zain <blackhawksrock9696@gmail.com>
2023-08-02 01:28:22 -0400

    Added StoreClick Listener

--- app/src/main/java/com/example/b07group7project/GetStoreInterface.java
+++ app/src/main/java/com/example/b07group7project/GetStoreInterface.java
@@ -1,7 +1,8 @@
 package com.example.b07group7project;
 
 import java.util.ArrayList;
-import java.util.List;
+
+
 
 public interface GetStoreInterface {
     public ArrayList<Store> getStores();
--- app/src/main/java/com/example/b07group7project/ShopperViewStoreFragment.java
+++ app/src/main/java/com/example/b07group7project/ShopperViewStoreFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -11,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class ShopperViewStoreFragment extends Fragment {
+public class ShopperViewStoreFragment extends Fragment implements StoreClickListener{
 
     public ShopperViewStoreFragment() {
         // Required empty public constructor
@@ -30,11 +31,17 @@ public class ShopperViewStoreFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.StoreListRecyclerView);
         // Line where getStores is used
         GetStoreInterface storeInterface = new GetStoreImplementation();
+        //
         ArrayList<Store> StoreList = storeInterface.getStores();
-        StoreRecyclerViewAdapter adapter = new StoreRecyclerViewAdapter(requireContext(), StoreList);
+        StoreRecyclerViewAdapter adapter = new StoreRecyclerViewAdapter(requireContext(), StoreList, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new GridLayoutManager(requireContext(), 2));
 
         return view;
+    }
+
+    @Override
+    public void onStoreClicked(Store store) {
+        Toast.makeText(requireContext(), store.getStoreName(), Toast.LENGTH_SHORT).show();
     }
 }
