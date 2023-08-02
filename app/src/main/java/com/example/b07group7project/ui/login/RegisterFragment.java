40e9f8f30a84f23d649d5e922712d26c794519fd
Aaron D'Mello <aaron.dmello@hotmail.com>
2023-08-02 11:34:50 -0400

    Extract logic out of RegisterFragment and ResetPasswordFragment

--- app/src/main/java/com/example/b07group7project/ui/login/RegisterFragment.java
+++ app/src/main/java/com/example/b07group7project/ui/login/RegisterFragment.java
@@ -4,19 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.b07group7project.UserType;
 import com.example.b07group7project.databinding.FragmentRegisterAccountBinding;
 
 public class RegisterFragment extends Fragment {
 
     FragmentRegisterAccountBinding binding;
-    LoginModel loginModel;
+    RegisterPresenter registerPresenter;
 
     @Nullable
     @Override
@@ -25,34 +23,16 @@ public class RegisterFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         binding = FragmentRegisterAccountBinding.inflate(getLayoutInflater());
-        loginModel = ((EmailPasswordActivity)requireActivity()).getLoginModel();
+        registerPresenter = new RegisterPresenter(this,
+                ((EmailPasswordActivity)requireActivity()).getLoginModel());
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.createShopperAccount.setOnClickListener(v-> handleCreateShopperAccount());
-        binding.createStoreOwnerAccount.setOnClickListener(v-> handleCreateStoreOwnerAccount());
+        binding.createShopperAccount.setOnClickListener(v-> registerPresenter.handleCreateShopperAccount());
+        binding.createStoreOwnerAccount.setOnClickListener(v-> registerPresenter.handleCreateStoreOwnerAccount());
 
-    }
-
-    private void handleCreateStoreOwnerAccount() {
-        if(loginModel.getCurrentUser() == null){
-            Toast.makeText(requireContext(), "Account creation failed", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        loginModel.setUserType(UserType.STORE_OWNER, loginModel.getCurrentUser());
-        ((EmailPasswordActivity) requireActivity()).moveToStoreOwnerLandingPage();
-    }
-
-    private void handleCreateShopperAccount() {
-        if(loginModel.getCurrentUser() == null){
-            Toast.makeText(requireContext(), "Account creation failed", Toast.LENGTH_LONG).show();
-            return;
-        }
-        loginModel.setUserType(UserType.SHOPPER, loginModel.getCurrentUser());
-        ((EmailPasswordActivity) requireActivity()).moveToShopperLandingPage();
     }
 }
