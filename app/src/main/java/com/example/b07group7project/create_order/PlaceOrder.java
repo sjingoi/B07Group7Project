e9d11b467611d4cb3aa3b379528467af33151fdb
Zain <blackhawksrock9696@gmail.com>
2023-08-04 09:15:47 -0400

    Fixed UI and Added Place Order Functionality

--- app/src/main/java/com/example/b07group7project/create_order/PlaceOrder.java
+++
@@ -1,8 +0,0 @@
-package com.example.b07group7project.create_order;
-
-import java.util.List;
-
-public interface PlaceOrder {
-    // TODO: Change User From String to Appropriate Type
-    public void placeOrder(List<Product> products, String user);
-}
