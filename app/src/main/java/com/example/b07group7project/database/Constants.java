f659f5c52cf2941721ca71ea1a4230668a4c00b6
Aaron D'Mello <aaron.dmello@hotmail.com>
2023-08-06 08:27:22 -0400

    (incomplete) template for getting cart

---
+++ app/src/main/java/com/example/b07group7project/database/Constants.java
@@ -0,0 +1,5 @@
+package com.example.b07group7project.database;
+
+public class Constants {
+    public static final String DATABASE_URL = "https://b07group7project-default-rtdb.firebaseio.com/";
+}
--- app/src/main/java/com/example/b07group7project/database/ProductDatabase.java
+++ app/src/main/java/com/example/b07group7project/database/ProductDatabase.java
@@ -6,8 +6,8 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.example.b07group7project.Store;
-import com.example.b07group7project.shopper_view_store.GetStoreInterface;
+import com.example.b07group7project.create_order.GetCartInterface;
+import com.example.b07group7project.create_order.Product;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,28 +18,32 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class StoreDatabase implements GetStoreInterface {
-    ArrayList<Store> stores;
+public class ProductDatabase implements GetCartInterface {
+
+
+    ArrayList<Product> products;
     FirebaseDatabase database;
     DatabaseReference reference;
 
 
-    public StoreDatabase(){
+    public ProductDatabase(){
         database = FirebaseDatabase.getInstance(DATABASE_URL);
         reference = database.getReference();
     }
-    public void getStores(OnComplete<ArrayList<Store>> onComplete) {
-
-        if(stores != null){
-            onComplete.onComplete(new ArrayList<>(stores));
+    @Override
+    public void getCart(OnComplete<ArrayList<Product>> onComplete) {
+        if(products != null){
+            onComplete.onComplete(new ArrayList<>(products));
             return;
         }
 
-        DatabaseReference databaseReference = reference.child("Stores");
+        DatabaseReference databaseReference = reference.child("Customers")
+                .child("f025806d-594f-443a-a146-d6d773274c4a")
+                .child("Cart");
         databaseReference.addValueEventListener((new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                updateStoreList(snapshot);
+                updateProductList(snapshot);
             }
 
             @Override
@@ -50,30 +54,30 @@ public class StoreDatabase implements GetStoreInterface {
 
         Task<DataSnapshot> dataSnapshotTask = databaseReference.get();
         dataSnapshotTask.addOnCompleteListener(v -> {
-            updateStoreList(v.getResult());
-            onComplete.onComplete(new ArrayList<>(stores));
+            updateProductList(v.getResult());
+            onComplete.onComplete(new ArrayList<>(products));
         });
     }
 
-    public void updateStoreList(DataSnapshot dataSnapshot){
+    private void updateProductList(DataSnapshot dataSnapshot) {
         Log.d("db", "updateStoreListDataSnap: " + dataSnapshot);
-        ArrayList<Store> store = new ArrayList<>();
+        ArrayList<Product> tempProducts = new ArrayList<>();
         for (DataSnapshot e : dataSnapshot.getChildren()) {
             Log.d("myLog", e.getKey());
             HashMap<String, Object> value = (HashMap<String, Object>) e.getValue();
             if(value == null)
                 continue;
             String name = getObjectAsString(
-                    value.getOrDefault("Store Name", null)
+                    value.getOrDefault("ProductName", null)
             );
-            String url = getObjectAsString(
-                    value.getOrDefault("Store Image", null)
-            );
+            double price = (double) value.get("Price");
+            int quantity = (int) value.get("Quantity");
 
-            store.add(new Store(name, url));
+
+            tempProducts.add(new Product(name, price, quantity));
         }
-        Log.d("myLog", "storeList: " + store.size());
-        stores = store;
+        Log.d("myLog", "storeList: " + tempProducts.size());
+        products = tempProducts;
     }
 
     private String getObjectAsString(Object o){
