d0532c23b0beb4ef93187302da9cf4e5565cc5d0
Hari Balamurali <haribala246@gmail.com>
2023-08-05 20:52:47 -0400

    Database-addCustomer, Changed productsuuidToQuantity type to Product, Integer

--- app/src/main/java/com/example/b07group7project/Order.java
+++ app/src/main/java/com/example/b07group7project/Order.java
@@ -6,17 +6,15 @@ import java.util.Map;
 import java.util.UUID;
 
 public class Order {
-    HashMap<Product, String> productsuuidToQuantity;
+    HashMap<Product, Integer> productsuuidToQuantity;
     Store store;
     Customer customer;
 
     String uuid;
 
-    public Order() {
 
-    }
 
-    public Order(HashMap<Product, String> productsuuidToQuantity, Store store, Customer customer) {
+    public Order(HashMap<Product, Integer> productsuuidToQuantity, Store store, Customer customer) {
         this.productsuuidToQuantity= productsuuidToQuantity;
         this.store = store;
         this.customer = customer;
@@ -27,14 +25,14 @@ public class Order {
         HashMap<String, Object> newhashmap = new HashMap<>();
         newhashmap.put(Constants.customer_uuid, customer.uuid);
         newhashmap.put(Constants.order_complete, "false");
-        HashMap<String, String> products = convertHashMap();
+        HashMap<String, Integer> products = convertHashMap();
         newhashmap.put(Constants.store_products, products);
         return newhashmap;
     }
 
-    private HashMap<String, String> convertHashMap() {
-        HashMap<String, String> newhashmap = new HashMap<>();
-        for (Map.Entry<Product, String> m : productsuuidToQuantity.entrySet()) {
+    private HashMap<String, Integer> convertHashMap() {
+        HashMap<String, Integer> newhashmap = new HashMap<>();
+        for (Map.Entry<Product, Integer> m : productsuuidToQuantity.entrySet()) {
             newhashmap.put(m.getKey().uuid, m.getValue());
         }
         return newhashmap;
