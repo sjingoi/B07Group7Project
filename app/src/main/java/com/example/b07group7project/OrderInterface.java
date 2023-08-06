bac6dbb6f8d7748f11d73ad4cb622400641e62dc
Jasjot S. Benipal <jasjot.benipal11@gmail.com>
2023-08-06 13:15:39 -0400

    Interface for orders

---
+++ app/src/main/java/com/example/b07group7project/OrderInterface.java
@@ -0,0 +1,14 @@
+package com.example.b07group7project;
+
+import java.util.List;
+
+public interface OrderInterface {
+    interface OrderCallback {
+        void onPendingOrdersLoaded(List<Order> orders);
+        void onOrderConfirmationSuccess();
+        void onOrderConfirmationFailure(String errorMessage);
+    }
+
+    void getPendingOrdersForStoreOwner(String storeOwnerId, OrderCallback callback);
+    void confirmOrder(Order order, OrderCallback callback);
+}
