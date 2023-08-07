5016fa5eb379513fbcf9eb8327f8dc406ba19d59
Aaron D'Mello <aaron.dmello@hotmail.com>
2023-08-07 15:21:21 -0400

    Create Generic database methods for getting with cache

--- app/src/main/java/com/example/b07group7project/database/StoreDatabase.java
+++ app/src/main/java/com/example/b07group7project/database/StoreDatabase.java
@@ -2,58 +2,33 @@ package com.example.b07group7project.database;
 
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-
 import com.example.b07group7project.database_abstractions.Store;
 import com.example.b07group7project.shopper_view_store.GetStoreInterface;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class StoreDatabase implements GetStoreInterface {
+public class StoreDatabase extends Database implements GetStoreInterface {
     ArrayList<Store> stores;
-    FirebaseDatabase database;
-    DatabaseReference reference;
-
 
     public StoreDatabase(){
-        database = FirebaseDatabase.getInstance("https://b07group7project-default-rtdb.firebaseio.com/");
-        reference = database.getReference();
+        super();
     }
     public void getStores(OnComplete<ArrayList<Store>> onComplete) {
-
         if(stores != null){
             onComplete.onComplete(new ArrayList<>(stores));
             return;
         }
 
-        DatabaseReference databaseReference = reference.child("Stores");
-        databaseReference.addValueEventListener((new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                updateStoreList(snapshot);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.d("db", error.toString());
-            }
-        }));
-
-        Task<DataSnapshot> dataSnapshotTask = databaseReference.get();
-        dataSnapshotTask.addOnCompleteListener(v -> {
-            updateStoreList(v.getResult());
-            onComplete.onComplete(new ArrayList<>(stores));
-        });
+        getWithCache(
+                root.child("Stores"),
+                onComplete,
+                this::updateStoreList
+        );
     }
 
-    public void updateStoreList(DataSnapshot dataSnapshot){
+    public ArrayList<Store> updateStoreList(DataSnapshot dataSnapshot){
         Log.d("db", "updateStoreListDataSnap: " + dataSnapshot);
         ArrayList<Store> store = new ArrayList<>();
         for (DataSnapshot e : dataSnapshot.getChildren()) {
@@ -71,12 +46,9 @@ public class StoreDatabase implements GetStoreInterface {
             store.add(new Store(name, url));
         }
         Log.d("myLog", "storeList: " + store.size());
-        stores = store;
+        stores = new ArrayList<>(store);
+        return store;
     }
 
-    private String getObjectAsString(Object o){
-        if(o == null)
-            return null;
-        return o.toString();
-    }
+
 }
