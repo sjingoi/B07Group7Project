2b4ff914b7179fb8e983fb9143714361b39879b6
Zain <blackhawksrock9696@gmail.com>
2023-08-07 15:58:39 -0400

    Display data on screen

--- app/src/main/java/com/example/b07group7project/shopper_view_previous_orders/ShopperPreviousOrderFragment.java
+++ app/src/main/java/com/example/b07group7project/shopper_view_previous_orders/ShopperPreviousOrderFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,24 +16,12 @@ import com.example.b07group7project.Customer;
 import com.example.b07group7project.R;
 import com.example.b07group7project.database.OnComplete;
 import com.example.b07group7project.shopper_view_store.ShopperViewStoreFragment;
+import com.example.b07group7project.shopper_view_store.StoreRecyclerViewAdapter;
 
 import java.util.ArrayList;
 
 public class ShopperPreviousOrderFragment extends Fragment {
 
-    ArrayList<PreviousOrder> previousOrders = new ArrayList<>();
-    /*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_previousorders);
-        RecyclerView previousorderview = findViewById(R.id.viewPreviousOrders);
-        setUpPreviousOrders();
-        PreviousOrderAdapter adapter = new PreviousOrderAdapter(this, previousOrders);
-        previousorderview.setAdapter(adapter);
-        previousorderview.setLayoutManager(new LinearLayoutManager(this));
-    }
-    */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +37,14 @@ public class ShopperPreviousOrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_previousorders, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.viewPreviousOrders);
+
         getPreviousOrderImplementation getPreviousOrders = new getPreviousOrderImplementation();
+        ArrayList<PreviousOrder> order = getPreviousOrders.getPreviousOrders();
+
+        PreviousOrderAdapter adapter = new PreviousOrderAdapter(requireContext(), order);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        
         return view;
     }
     /*
