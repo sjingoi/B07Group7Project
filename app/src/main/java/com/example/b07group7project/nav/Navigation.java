55c11d50375b4195d4038e50d8f0ae65ae97cf5d
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-08-08 15:06:51 -0400

    added replaceFragment bundle

--- app/src/main/java/com/example/b07group7project/nav/Navigation.java
+++ app/src/main/java/com/example/b07group7project/nav/Navigation.java
@@ -22,7 +22,7 @@ public abstract class Navigation extends AppCompatActivity {
             fragmentTransaction.setCustomAnimations(R.anim.enter_right_to_left, R.anim.exit_right_to_left, R.anim.enter_left_to_right, R.anim.exit_left_to_right);
         }
         if (bundle != null) {
-
+            newFragment.setArguments(bundle);
         }
         fragmentTransaction.replace(getFragmentContainer(), newFragment);
         fragmentTransaction.commit();
