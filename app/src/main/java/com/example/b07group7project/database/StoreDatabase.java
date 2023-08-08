ac3290985747ea864ee30638eeea3948aee742e4
Aaron D'Mello <aaron.dmello@hotmail.com>
2023-08-08 18:41:05 -0400

    remove logging, do not cast to DataSnapshot

--- app/src/main/java/com/example/b07group7project/database/StoreDatabase.java
+++ app/src/main/java/com/example/b07group7project/database/StoreDatabase.java
@@ -1,7 +1,5 @@
 package com.example.b07group7project.database;
 
-import android.util.Log;
-
 import com.example.b07group7project.database_abstractions.StoreHeader;
 import com.example.b07group7project.shopper_view_store.GetStoreInterface;
 import com.google.firebase.database.DataSnapshot;
@@ -29,20 +27,13 @@ public class StoreDatabase extends Database implements GetStoreInterface {
     }
 
     public ArrayList<StoreHeader> updateStoreList(DataSnapshot dataSnapshot){
-        Log.d("db", "updateStoreListDataSnap: " + dataSnapshot);
         ArrayList<StoreHeader> store = new ArrayList<>();
         for (DataSnapshot e : dataSnapshot.getChildren()) {
-            Log.d("myLog", e.getKey());
-            DataSnapshot value = e.getValue(DataSnapshot.class);
-            if(value == null)
-                continue;
-            String name = value.child(Constants.store_name).getValue(String.class);
-            String url = value.child(Constants.store_image).getValue(String.class);
+            String name = e.child(Constants.store_name).getValue(String.class);
+            String url = e.child(Constants.store_image).getValue(String.class);
             String uuid = e.getKey();
-
             store.add(new StoreHeader(name, url, uuid));
         }
-        Log.d("myLog", "storeList: " + store.size());
         stores = new ArrayList<>(store);
         return store;
     }
