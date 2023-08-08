e97807e7f1f56bc2e30e0b76ba3579a7858a52dd
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-08-08 14:58:30 -0400

    added replaceFragment bundle

--- app/src/main/java/com/example/b07group7project/nav/Navigation.java
+++ app/src/main/java/com/example/b07group7project/nav/Navigation.java
@@ -1,5 +1,7 @@
 package com.example.b07group7project.nav;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -10,17 +12,24 @@ public abstract class Navigation extends AppCompatActivity {
 
     public abstract int getFragmentContainer();
 
-    public void replaceFragment(Fragment newFragment, boolean addToBackStack) {
+    public void replaceFragment(Fragment newFragment, boolean addToBackStack, Bundle bundle) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         if (addToBackStack) {
             fragmentTransaction.addToBackStack(null);
         }
+        if (bundle != null) {
+
+        }
         fragmentTransaction.replace(getFragmentContainer(), newFragment);
         fragmentTransaction.commit();
     }
 
+    public void replaceFragment(Fragment newFragment, boolean addToBackStack) {
+        replaceFragment(newFragment, addToBackStack, null);
+    }
+
     public void replaceFragment(Fragment newFragment) {
-        replaceFragment(newFragment, false);
+        replaceFragment(newFragment, false, null);
     }
 }
