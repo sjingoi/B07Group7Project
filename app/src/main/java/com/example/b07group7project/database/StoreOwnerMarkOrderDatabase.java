de59820cfdb0b8291ac7d1b92f00271c24575929
Hari Balamurali <haribala246@gmail.com>
2023-08-09 13:22:42 -0400

    Completed implementation of markOrderAsComplete

--- app/src/main/java/com/example/b07group7project/database/StoreOwnerMarkOrderDatabase.java
+++ app/src/main/java/com/example/b07group7project/database/StoreOwnerMarkOrderDatabase.java
@@ -2,10 +2,8 @@ package com.example.b07group7project.database;
 
 
 
-import com.example.b07group7project.Product;
-
+import com.example.b07group7project.Order;
 import com.example.b07group7project.database_abstractions.StoreProduct;
-import com.example.b07group7project.mark_order.StoreOrder;
 import com.example.b07group7project.shopper_view_previous_orders.OrderStatus;
 import com.example.b07group7project.shopper_view_previous_orders.OrderedProduct;
 import com.google.firebase.database.DataSnapshot;
@@ -14,47 +12,45 @@ import com.google.firebase.database.DatabaseReference;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class StoreOwnerMarkOrderDatabase extends Database{
-    ArrayList<StoreOrder> orders;
+
     public StoreOwnerMarkOrderDatabase (){super();}
 
-    public void getStoreOwnerOrders(User user, OnComplete<ArrayList<StoreOrder>> onComplete) {
+    public void getStoreOwnerOrders(User user, OnComplete<ArrayList<OrderedProduct>> onComplete) {
         DatabaseReference newreference = root.child(Constants.store_orders).child(user.uuid);
         get(newreference, snapshot -> {
-            ArrayList<StoreOrder> storeOrders = new ArrayList<>();
+            ArrayList<OrderedProduct> storeOrders = new ArrayList<>();
             for (DataSnapshot storeOrder: snapshot.getChildren()) {
-                ArrayList<OrderedProduct> products = new ArrayList<>();
-                for (DataSnapshot product: storeOrder.child(Constants.store_products).getChildren()) {
-                    String productName = (String) product.child(Constants.product_name).getValue();
-                    String productUUID = (String) product.child(Constants.product_uuid).getValue();
-                    String productDescription = (String) product.child(Constants.product_description).getValue();
-                    String imageURL = (String) product.child(Constants.product_image).getValue();
-                    int price = (Integer) product.child(Constants.product_price).getValue();
-                    StoreProduct product1 = new StoreProduct(productName, productDescription,imageURL, price, productUUID);
-                    int quantity = (Integer) product.child(Constants.quantity).getValue();
-                    String orderStatus = (String) storeOrder.child(Constants.order_status).getValue();
-                    String customerUUID = (String) storeOrder.child(Constants.customer_uuid).getValue();
-                    OrderedProduct orderedProduct = new OrderedProduct(product1, orderStatus, quantity, customerUUID);
-                    products.add(orderedProduct);
-                }
-                StoreOrder storeOrder1 = new StoreOrder(products, storeOrder.getKey());
-                storeOrders.add(storeOrder1);
+                String productName = (String) storeOrder.child(Constants.product_name).getValue();
+                String productUUID = (String) storeOrder.child(Constants.product_uuid).getValue();
+                String productDescription = (String) storeOrder.child(Constants.product_description).getValue();
+                String imageURL = (String) storeOrder.child(Constants.product_image).getValue();
+                int price = (Integer) storeOrder.child(Constants.product_price).getValue();
+                StoreProduct product1 = new StoreProduct(productName, productDescription,imageURL, price, productUUID);
+                int quantity = (Integer) storeOrder.child(Constants.quantity).getValue();
+                String orderStatus = (String) storeOrder.child(Constants.order_status).getValue();
+                String customerUUID = (String) storeOrder.child(Constants.customer_uuid).getValue();
+                String orderUUID = (String) storeOrder.getKey().split(":")[1];
+                OrderedProduct orderedProduct = new OrderedProduct(product1, orderStatus, quantity, customerUUID, orderUUID);
+                storeOrders.add(orderedProduct);
             }
             onComplete.onComplete(storeOrders);
         });
     }
 
-    public void markOrderAsComplete(User user, OrderedProduct orderedProduct, StoreOrder storeOrder)  {
+    public void markOrderAsComplete(User user, OrderedProduct orderedProduct)  {
         AccountDatabase accountDatabase = new AccountDatabase();
-        accountDatabase.getStoreUUID(user, uuid -> onReceiveStoreUUID(orderedProduct, storeOrder, uuid));
+        accountDatabase.getStoreUUID(user, uuid -> onReceiveStoreUUID(orderedProduct, uuid));
     }
 
-    public void onReceiveStoreUUID(OrderedProduct orderedProduct, StoreOrder storeOrder, String storeUUID) {
-        DatabaseReference reference1 = root.child(Constants.store_orders).child(storeUUID).child(storeOrder.getCurrentDate()).child(Constants.order_status);
+    public void onReceiveStoreUUID(OrderedProduct orderedProduct, String storeUUID) {
+        DatabaseReference reference1 = root.child(Constants.store_orders).child(storeUUID).child(orderedProduct.getCurrentDate()).child(Constants.order_status);
         put(reference1, snapshot -> OrderStatus.ORDER_COMPLETE.toString());
-        DatabaseReference reference2 = root.child(Constants.customers).child(storeOrder.getCustomerUUID()).child(Constants.previous_orders).child(Constants.current_date).child(orderedProduct.getProduct().getStoreProductUUID());
+        DatabaseReference reference2 = root.child(Constants.customers).child(orderedProduct.getCustomerUUID()).
+                child(Constants.previous_orders).child(Constants.current_date).child(orderedProduct.getProduct().getStoreProductUUID());
         put(reference2, snapshot -> OrderStatus.ORDER_COMPLETE.toString());
     }
 
--- app/src/main/java/com/example/b07group7project/shopper_view_previous_orders/OrderedProduct.java
+++ app/src/main/java/com/example/b07group7project/shopper_view_previous_orders/OrderedProduct.java
@@ -3,23 +3,36 @@ package com.example.b07group7project.shopper_view_previous_orders;
 import com.example.b07group7project.Product;
 import com.example.b07group7project.database_abstractions.StoreProduct;
 
+import java.util.UUID;
+
 public class OrderedProduct {
     StoreProduct product;
     String orderStatus;
     int quantity;
     String customerUUID;
+    String orderUUID;
+    String currentDate;
 
-    public OrderedProduct(StoreProduct product, String orderStatus, int quantity, String customerUUID) {
+    public OrderedProduct(StoreProduct product, String orderStatus, int quantity, String customerUUID, String orderUUID) {
         this.product = product;
         this.orderStatus = orderStatus;
         this.quantity = quantity;
         this.customerUUID = customerUUID;
+        this.orderUUID = orderUUID;
     }
 
     public OrderedProduct(StoreProduct product, OrderStatus orderStatus, int quantity) {
         this.product = product;
         this.orderStatus = orderStatus.toString();
         this.quantity = quantity;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public String getCustomerUUID() {
+        return customerUUID;
     }
 
     public OrderedProduct(StoreProduct product, String orderStatus, int quantity) {
