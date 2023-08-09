81cc8e57d2b92ab505a26b44c4088a36585decff
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-08-08 21:12:41 -0400

    Resolved Merge Conflicts

--- app/src/main/java/com/example/b07group7project/nav/StoreOwnerNavigationActivity.java
+++ app/src/main/java/com/example/b07group7project/nav/StoreOwnerNavigationActivity.java
@@ -9,7 +9,6 @@ import com.example.b07group7project.ExampleFragment;
 import com.example.b07group7project.R;
 import com.example.b07group7project.create_product.CreateProductFragment;
 import com.example.b07group7project.databinding.ActivityStoreOwnerNavigationBinding;
-import com.example.b07group7project.store_owner_view_store.StoreOwnerViewProducts;
 
 public class StoreOwnerNavigationActivity extends Navigation {
 
@@ -33,7 +32,7 @@ public class StoreOwnerNavigationActivity extends Navigation {
 
         toolbar = findViewById(R.id.toolbar);
 
-        homeFragment = ExampleFragment.newInstance();    // CHANGE THIS
+        homeFragment = StoreOwnerViewProducts.newInstance();   // CHANGE THIS
         cartFragment = ExampleFragment.newInstance();    // CHANGE THIS
         accountFragment = CreateProductFragment.newInstance(); // CHANGE THIS
 
