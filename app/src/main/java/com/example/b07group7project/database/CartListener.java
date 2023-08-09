4150bb80769f5e511eecc834b140b5ab28c89db3
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-08-09 04:39:19 -0400

    Added required cart database methods

--- app/src/main/java/com/example/b07group7project/database/CartListener.java
+++ app/src/main/java/com/example/b07group7project/database/CartListener.java
@@ -2,4 +2,6 @@ package com.example.b07group7project.database;
 
 public interface CartListener {
     void addToCart(String storeuuid, String customeruuid, String itemID, int quantity);
+
+    void removeFromCart(String storeuuid, String customeruuid, String itemID);
 }
