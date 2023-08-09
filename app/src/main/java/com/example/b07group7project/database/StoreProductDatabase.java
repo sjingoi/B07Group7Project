cb80f7d02df791d52b5ca460a39f1b646907a7ee
Zain <blackhawksrock9696@gmail.com>
2023-08-08 23:42:01 -0400

    Implemented Database thing to get product from UUID

--- app/src/main/java/com/example/b07group7project/database/StoreProductDatabase.java
+++ app/src/main/java/com/example/b07group7project/database/StoreProductDatabase.java
@@ -1,12 +1,14 @@
 package com.example.b07group7project.database;
 
+import com.example.b07group7project.database_abstractions.Store;
 import com.example.b07group7project.database_abstractions.StoreProduct;
+import com.example.b07group7project.itempreview.GetProductInfo;
 import com.example.b07group7project.view_products.GetProductsInterface;
 import com.google.firebase.database.DataSnapshot;
 
 import java.util.ArrayList;
 
-public class StoreProductDatabase extends Database implements GetProductsInterface {
+public class StoreProductDatabase extends Database implements GetProductsInterface, GetProductInfo {
 
     public StoreProductDatabase(){
         super();
@@ -48,4 +50,48 @@ public class StoreProductDatabase extends Database implements GetProductsInterfa
 
         return products;
     }
+
+
+    // Code For Getting Individual Product
+    @Override
+    public void getProductFromFirebase(String storeUUID, String itemUUID, OnComplete<StoreProduct> withProduct) {
+        if(storeUUID == null){
+            withProduct.onComplete(new StoreProduct());
+            return;
+        }
+
+        get(
+                root.child("Product").child(storeUUID),
+                snapshot -> {
+                    StoreProduct Product = extractProduct(storeUUID, itemUUID, snapshot);
+                    withProduct.onComplete(Product);
+                }
+        );
+    }
+
+
+    private StoreProduct extractProduct(String storeUUID, String itemUUID, DataSnapshot snapshot){
+        StoreProduct product = new StoreProduct();
+        if(!snapshot.exists())
+            return product;
+        for (DataSnapshot s: snapshot.getChildren()) {
+            String uuid = s.getKey();
+
+            String description = s.child(Constants.product_description).getValue(String.class);
+            String imageURL = s.child(Constants.product_image).getValue(String.class);
+            String name = s.child(Constants.product_name).getValue(String.class);
+            Double price = s.child(Constants.product_price).getValue(Double.class);
+            if(price == null)
+                continue;
+
+            if(uuid == itemUUID) {
+                product = new StoreProduct(name, uuid, storeUUID, description, imageURL, price);
+            }
+
+
+        }
+
+        return product;
+    }
+
 }
