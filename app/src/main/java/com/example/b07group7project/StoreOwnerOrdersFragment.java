fc99d3feec3f8d7becb709954c7ef674add23e7c
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-08-09 20:39:31 -0400

    Added no items text to store owner orders

--- app/src/main/java/com/example/b07group7project/StoreOwnerOrdersFragment.java
+++ app/src/main/java/com/example/b07group7project/StoreOwnerOrdersFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
@@ -25,6 +26,8 @@ public class StoreOwnerOrdersFragment extends Fragment {
     private List<OrderedProduct> pendingOrdersList = new ArrayList<>();
     private OrderAdapter orderAdapter;
 
+    TextView textView;
+
     public StoreOwnerOrdersFragment() {
 
     }
@@ -40,6 +43,8 @@ public class StoreOwnerOrdersFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_store_owner_orders, container, false);
+
+        textView = rootView.findViewById(R.id.noProductsText);
 
         RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
@@ -69,6 +74,11 @@ public class StoreOwnerOrdersFragment extends Fragment {
          */
     }
     private void updateUIWithOrders(List<OrderedProduct> orders) {
+        if (orders.size() == 0) {
+            textView.setVisibility(View.VISIBLE);
+        } else {
+            textView.setVisibility(View.INVISIBLE);
+        }
         pendingOrdersList.clear();
         pendingOrdersList.addAll(orders);
         orderAdapter.notifyDataSetChanged();
